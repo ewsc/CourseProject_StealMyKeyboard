ef4c9af2870601134769857da9266ab26189858b
gthanksg <alamovazamjon@gmail.com>
2023-10-19 00:32:05 +0300

    ide configurations

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: X:/1Third_Year/SP_CW
+# Install script for directory: X:/1Third_Year/CourseProject_StealMyKeyboard
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "X:/1Third_Year/SP_CW/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "X:/1Third_Year/CourseProject_StealMyKeyboard/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
