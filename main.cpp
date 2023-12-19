604614cccb98b32a1c3e3effdbc7becdd4384767
ewsc <alamovazamjon@gmail.com>
2023-12-19 22:02:43 +0300

    domain blockage added to window

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <Windows.h>
+
+HWND hwndBlockButton;
+HWND hwndUnblockButton;
+HWND hwndDomainInput;
+HWND hwndIpAddressInput;
 
 bool AddHostEntry(const std::string& domain, const std::string& ipAddress)
 {
@@ -42,51 +48,158 @@ bool RemoveHostEntry(const std::string& domain, const std::string& ipAddress)
     return false;
 }
 
-int main()
+LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    std::string domain;
-    std::string ipAddress;
-
-    std::cout << "Menu:\n";
-    std::cout << "1. Block domain\n";
-    std::cout << "2. Unblock domain\n";
-    std::cout << "Enter your choice: ";
-    int choice;
-    std::cin >> choice;
-
-    switch (choice)
+    if (nCode >= 0)
     {
-        case 1:
-            std::cout << "Enter the domain to block: ";
-            std::cin >> domain;
-            ipAddress = "0.0.0.0";
-            if (AddHostEntry(domain, ipAddress))
+        auto* pKeyboardStruct = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+        DWORD vkCode = pKeyboardStruct->vkCode;
+
+        if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
+        {
+            // Handle the key press
+            std::cout << "Key pressed: " << vkCode << std::endl;
+        }
+    }
+
+    return CallNextHookEx(nullptr, nCode, wParam, lParam);
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        case WM_CREATE:
+        {
+            CreateWindow("STATIC", "Enter the domain and IP address to block/unblock:",
+                         WS_VISIBLE | WS_CHILD,
+                         20, 20, 360, 20,
+                         hwnd, NULL, NULL, NULL);
+
+            hwndDomainInput = CreateWindow("EDIT", "",
+                                           WS_VISIBLE | WS_CHILD | WS_BORDER,
+                                           20, 50, 200, 30,
+                                           hwnd, NULL, NULL, NULL);
+
+            hwndIpAddressInput = CreateWindow("EDIT", "",
+                                              WS_VISIBLE | WS_CHILD | WS_BORDER,
+                                              20, 90, 200, 30,
+                                              hwnd, NULL, NULL, NULL);
+
+            hwndBlockButton = CreateWindow("BUTTON", "Block",
+                                           WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                                           20, 130, 150, 30,
+                                           hwnd, NULL, NULL, NULL);
+
+            hwndUnblockButton = CreateWindow("BUTTON", "Unblock",
+                                             WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                                             200, 130, 150, 30,
+                                             hwnd, NULL, NULL, NULL);
+
+            HINSTANCE hInstance = reinterpret_cast<HINSTANCE>(GetWindowLongPtr(hwnd, GWLP_HINSTANCE));
+
+            // Install the keyboard hook
+            HHOOK hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, hInstance, 0);
+            if (hKeyboardHook == NULL)
             {
-                std::cout << "Domain blocked successfully." << std::endl;
+                MessageBox(hwnd, "Failed to install the keyboard hook.", "Error", MB_OK | MB_ICONERROR);
             }
             else
             {
-                std::cout << "Failed to block domain." << std::endl;
+                // Store the hook handle in the window user data
+                SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(hKeyboardHook));
             }
+        }
             break;
-        case 2:
-            std::cout << "Enter the domain to unblock: ";
-            std::cin >> domain;
-            std::cout << "Enter the IP address to unblock: ";
-            std::cin >> ipAddress;
-            if (RemoveHostEntry(domain, ipAddress))
+
+        case WM_COMMAND:
+        {
+            if (lParam == (LPARAM)hwndBlockButton)
             {
-                std::cout << "Domain unblocked successfully." << std::endl;
+                char domain[256];
+                char ipAddress[256];
+
+                GetWindowTextA(hwndDomainInput, domain, sizeof(domain));
+                GetWindowTextA(hwndIpAddressInput, ipAddress, sizeof(ipAddress));
+
+                if (AddHostEntry(domain, ipAddress))
+                {
+                    MessageBox(hwnd, "Domain blocked successfully.", "Success", MB_OK | MB_ICONINFORMATION);
+                }
+                else
+                {
+                    MessageBox(hwnd, "Failed to block domain.", "Error", MB_OK | MB_ICONERROR);
+                }
             }
-            else
+            else if (lParam == (LPARAM)hwndUnblockButton)
             {
-                std::cout << "Failed to unblock domain." << std::endl;
+                char domain[256];
+                char ipAddress[256];
+
+                GetWindowTextA(hwndDomainInput, domain, sizeof(domain));
+                GetWindowTextA(hwndIpAddressInput, ipAddress, sizeof(ipAddress));
+
+                if (RemoveHostEntry(domain, ipAddress))
+                {
+                    MessageBox(hwnd, "Domain unblocked successfully.", "Success", MB_OK | MB_ICONINFORMATION);
+                }
+                else
+                {
+                    MessageBox(hwnd, "Failed to unblock domain.", "Error", MB_OK | MB_ICONERROR);
+                }
             }
+        }
             break;
+
+        case WM_DESTROY:
+        {
+            // Uninstall the keyboard hook
+            HHOOK hKeyboardHook = reinterpret_cast<HHOOK>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+            UnhookWindowsHookEx(hKeyboardHook);
+            PostQuitMessage(0);
+        }
+            break;
+
         default:
-            std::cout << "Invalid choice." << std::endl;
-            break;
+            return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
 
     return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // Register the window class
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = "BlockDomainWindowClass";
+
+    if (!RegisterClass(&wc))
+    {
+        MessageBox(NULL, "Window registration failed.", "Error", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    // Create the window
+    HWND hwnd = CreateWindow(wc.lpszClassName, "Block Domain",
+                             WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                             CW_USEDEFAULT, CW_USEDEFAULT, 400, 250,
+                             NULL, NULL, hInstance, NULL);
+
+    if (hwnd == NULL)
+    {
+        MessageBox(NULL, "Window creation failed.", "Error", MB_OK | MB_ICONERROR);
+        return 1;
+    }
+
+    // Message loop
+    MSG msg;
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return static_cast<int>(msg.wParam);
 }
