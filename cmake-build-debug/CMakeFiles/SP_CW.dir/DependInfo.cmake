bc6a72ca687cb242aeed339e7568fb4747a254e1
ewsc <alamovazamjon@gmail.com>
2023-12-20 02:58:18 +0300

    fin

--- cmake-build-debug/CMakeFiles/SP_CW.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/SP_CW.dir/DependInfo.cmake
@@ -11,8 +11,8 @@ set(CMAKE_DEPENDS_DEPENDENCY_FILES
   "X:/1Third_Year/CourseProject_StealMyKeyboard/main.cpp" "CMakeFiles/SP_CW.dir/main.cpp.obj" "gcc" "CMakeFiles/SP_CW.dir/main.cpp.obj.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
--- test_functions.cpp
+++ test_functions.cpp
@@ -1,51 +1,30 @@
 #include <iostream>
-#include <Windows.h>
-#include "include/windivert.h"
+#include <windows.h>
+#include <Iphlpapi.h>
+#include <vector>
 
-// Function to block internet access
-void BlockInternetAccess()
-{
-    // Create a handle to the WinDivert driver
-    HANDLE handle = WinDivertOpen("outbound && ip", WINDIVERT_LAYER_NETWORK, 0, 0);
-    if (handle == INVALID_HANDLE_VALUE)
-    {
-        std::cerr << "Failed to open WinDivert driver. Make sure you have administrative privileges." << std::endl;
-        return;
+#pragma comment(lib, "iphlpapi.lib")
+
+int main() {
+    DWORD bufferSize = 0;
+    DWORD status = GetExtendedTcpTable(nullptr, &bufferSize, false, AF_INET, TCP_TABLE_OWNER_MODULE_ALL, 0);
+    if (status != ERROR_INSUFFICIENT_BUFFER) {
+        std::cerr << "Failed to retrieve TCP table size: " << status << std::endl;
+        return 1;
     }
 
-    // Modify the network packets to block internet access
-    char packet[65536];
-    UINT packetLength;
-
-    while (true)
-    {
-        if (!WinDivertRecv(handle, packet, sizeof(packet), &packetLength, NULL))
-        {
-            std::cerr << "Error while receiving packet: " << GetLastError() << std::endl;
-            break;
-        }
-
-        // Drop the packet to block internet access
-        if (!WinDivertSend(handle, packet, packetLength, NULL, NULL))
-        {
-            std::cerr << "Error while sending packet: " << GetLastError() << std::endl;
-            break;
-        }
+    std::vector<byte> buffer(bufferSize);
+    PMIB_TCPTABLE_OWNER_MODULE tcpTable = reinterpret_cast<PMIB_TCPTABLE_OWNER_MODULE>(buffer.data());
+    status = GetExtendedTcpTable(tcpTable, &bufferSize, false, AF_INET, TCP_TABLE_OWNER_MODULE_ALL, 0);
+    if (status != NO_ERROR) {
+        std::cerr << "Failed to retrieve TCP table: " << status << std::endl;
+        return 1;
     }
 
-    // Close the handle to the WinDivert driver
-    WinDivertClose(handle);
-}
-
-// Entry point of the program
-int main()
-{
-    // Create a button-click event mechanism
-    std::cout << "Press any key to block internet access...";
-    std::cin.ignore();
-
-    // Block internet access when the button is clicked
-    BlockInternetAccess();
+    for (DWORD i = 0; i < tcpTable->dwNumEntries; ++i) {
+        PMIB_TCPROW_OWNER_MODULE row = &tcpTable->table[i];
+        std::cout << "Accessed Domain: " << row->dwRemoteAddr << std::endl;
+    }
 
     return 0;
 }
